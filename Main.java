a0f9465fa92fdc1498b901edc959f7ed2594d983
Latifa M <94848902+l-atifa@users.noreply.github.com>
2022-06-13 13:20:04 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,82 @@
+class Main {
+
+  
+  /*public static void puff(int n)
+  {
+    if(n==1)
+    {
+      System.out.print("x");
+    }
+    else if(n%2 == 0)
+    {
+      System.out.print("{");
+      puff(n-1);
+      System.out.print("}");
+    }
+     else
+    {
+      System.out.print("<");
+      puff(n-1);
+      System.out.print(">");
+    }
+  }*/
+  
+  public static void main(String[] args) 
+  {
+    String str = "prose";
+    System.out.println(str.substring(1) + "&");
+    System.out.println("emerge".compareTo("hat"));
+
+    str.toUpperCase();
+    System.out.println(str);
+    /*int[][] grid = {{1, 5, 2},{3, 8, 6},{9, 4, 7},{4, 6, 1}};
+
+    System.out.println(grid);
+
+    for(int r = 0; r<grid.length; r++)
+      {
+        for(int c = 0; c<grid[0].length; c++)
+          {
+            System.out.println()
+          }
+      }
+  }*/
+
+    
+//can only print specified point in single statenebt
+//cannot print whole row or col
+
+    //magpie testing
+    /*String sen = " I am going to school tomorrow!";
+    System.out.println(sen);
+    System.out.println(sen.trim());*/
+
+    //unit 10 question
+    //puff(4);
+    
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
